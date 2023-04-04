82ce64db3494e4824aa9991fbab7ceba97a68f04
yurong4 <125841515+yurong4@users.noreply.github.com>
2023-04-04 16:47:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import gradio as gr
-def greet(name):
-    return "Hello" + name + "!!"
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+# import gradio as gr
+# def greet(name):
+#     return "Hello" + name + "!!"
+# iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+# iface.launch()
 
