5a937b677bf664eebe405317a03c3f78b84a7eab
yurong4 <125841515+yurong4@users.noreply.github.com>
2023-04-23 16:56:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@
 # iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 # iface.launch()
 
-import gradio as gr
-from transformers import pipelinexxxxx
+# import gradio as gr
+# from transformers import pipeline
 
-pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
+# pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
 
-def predict(image):
-    predictions = pipeline(image)
-    return {p["label"]: p["score"] for p in predictions}
+# def predict(image):
+#     predictions = pipeline(image)
+#     return {p["label"]: p["score"] for p in predictions}
 
-gr.Interface(
-    predict,
-    inputs=gr.inputs.Image(label="Upload hot dog candidate", type="filepath"),
-    outputs=gr.outputs.Label(num_top_classes=2),
-    title="Hot Dog? Or Not?",
-).launch()
+# gr.Interface(
+#     predict,
+#     inputs=gr.inputs.Image(label="Upload hot dog candidate", type="filepath"),
+#     outputs=gr.outputs.Label(num_top_classes=2),
+#     title="Hot Dog? Or Not?",
+# ).launch()
